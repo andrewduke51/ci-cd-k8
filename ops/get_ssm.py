7b04e0dc28f6f253d41596fb8929889ffb9a49a5
Andrew Figaroa <andrew.figaroa@medicasoftllc.com>
2020-08-25 19:44:35 -0400

    trigger 9

--- ops/get_ssm.py
+++ ops/get_ssm.py
@@ -1,12 +1,11 @@
 import boto3
-from pathlib import Path
 
 ssm = boto3.client('ssm', region_name='us-east-1')
 parameter = ssm.get_parameter(Name='/cicd/ssh_key', WithDecryption=True)
 FETCHED_SECRET=(parameter['Parameter']['Value'])
 print(FETCHED_SECRET)
 
-filename = Path("/root/.ssh/id_rsa")
+filename = "/root/.ssh/id_rsa"
 
 outF = open(filename, 'w+')
 for line in FETCHED_SECRET:
