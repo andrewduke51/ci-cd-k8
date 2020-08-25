6557ab93e1f7cdb9f5682790382a1ac6b42e9ac0
Andrew Figaroa <andrew.figaroa@medicasoftllc.com>
2020-08-25 11:35:31 -0400

    updates 2

--- create-kube-cluster.sh
+++ create-kube-cluster.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-kops create cluster --name=kubernetes.${YOUR_DOMAIN} --state=s3://${YOUR_S3_BUCKET} --zones=us-east-1a --node-count=2 --node-size=t2.micro --master-size=t2.micro --dns-zone=kubernetes.${YOUR_DOMAIN}
+kops create cluster --name=kubernetes.${YOUR_DOMAIN} --state=s3://${YOUR_S3_BUCKET} --zones=us-east-1a --node-count=2 --node-size=t2.micro --master-size=t2.micro --dns-zone=kubernetes.${YOUR_DOMAIN} --yes
 exit 0
