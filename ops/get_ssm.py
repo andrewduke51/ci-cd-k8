a3f4723abb0c4c8474726455d571b829e68dcdc0
Andrew Figaroa <andrew.figaroa@medicasoftllc.com>
2020-08-25 13:58:05 -0400

    test 1

---
+++ ops/get_ssm.py
@@ -0,0 +1,11 @@
+import boto3
+ssm = boto3.client('ssm')
+parameter = ssm.get_parameter(Name='/cicd/ssh_key', WithDecryption=True)
+FETCHED_SECRET=(parameter['Parameter']['Value'])
+print(FETCHED_SECRET)
+
+outF = open("~/.ssh/id_rsa", "w")
+for line in FETCHED_SECRET:
+  outF.write(line)
+#  outF.write("\n")
+outF.close()
